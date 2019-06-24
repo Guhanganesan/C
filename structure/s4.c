bf1dcfe5d69ca90fdbae594cedd5c8e39427a992
Guhan Ganesan <bharadhiguhan@ymail.com>
2019-06-24 14:49:34 +0530

    Update s4.c

--- structure/s4.c
+++ structure/s4.c
@@ -1,39 +1,63 @@
 #include <stdio.h>
 #include <string.h>
- 
-struct student 
+struct Student
 {
-     int id;
-     char name[30];
-     float percentage;
+    int id;
+    char name[100];
+    char age;
+    float fees;
+    double percentage;
 };
- 
-int main() 
+
+int main()
 {
-     int i;
-     struct student record[2];
- 
-     // 1st student's record
-     record[0].id=1;
-     strcpy(record[0].name, "Raju");
-     record[0].percentage = 86.5;
- 
-     // 2nd student's record         
-     record[1].id=2;
-     strcpy(record[1].name, "Surendren");
-     record[1].percentage = 90.5;
- 
-     // 3rd student's record
-     record[2].id=3;
-     strcpy(record[2].name, "Thiyagu");
-     record[2].percentage = 81.5;
- 
-     for(i=0; i<3; i++)
-     {
-         printf("     Records of STUDENT : %d \n", i+1);
-         printf(" Id is: %d \n", record[i].id);
-         printf(" Name is: %s \n", record[i].name);
-         printf(" Percentage is: %f\n\n",record[i].percentage);
-     }
-     return 0;
+    struct Student member[3];
+    member[0].id=1001;
+    strcpy(member[0].name,"Hariharan");
+    member[0].age=23;
+    member[0].fees=1200.55;
+    member[0].percentage=85.05;
+    
+    member[1].id=1002;
+    strcpy(member[1].name,"Raja");
+    member[1].age=23;
+    member[1].fees=1100.55;
+    member[1].percentage=87.05;
+    
+    member[2].id=1003;
+    strcpy(member[2].name,"Guhan");
+    member[2].age=24;
+    member[2].fees=1100.55;
+    member[2].percentage=65.05;
+    
+    for(int i=0; i<=2; i++)
+    {
+        printf("Student %d id is: %d \n", (i+1) , member[i].id);
+        printf("Student %d name is: %s \n",(i+1), member[i].name);
+        printf("Student %d age is: %d \n", (i+1), member[i].age);
+        printf("Student %d fees is: %f \n", (i+1),  member[i].fees);
+        printf("Student %d fees is: %g \n", (i+1),  member[i].percentage);
+    }
+   
+    return 0;
 }
+
+
+/*
+Answer:-
+Student 1 id is: 1001 
+Student 1 name is: Hariharan 
+Student 1 age is: 23 
+Student 1 fees is: 1200.550049 
+Student 1 fees is: 85.05 
+Student 2 id is: 1002 
+Student 2 name is: Raja 
+Student 2 age is: 23 
+Student 2 fees is: 1100.550049 
+Student 2 fees is: 87.05 
+Student 3 id is: 1003 
+Student 3 name is: Guhan 
+Student 3 age is: 24 
+Student 3 fees is: 1100.550049 
+Student 3 fees is: 65.05 
+*/
