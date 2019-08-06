e7d91bfe9ce05fc3b948717470ef5f898a686804
Guhan Ganesan <bharadhiguhan@ymail.com>
2019-08-06 18:02:56 +0530

    Update p2.c

--- Pointer/p2.c
+++ Pointer/p2.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
 
-int main () {
+int main()
+{
+    int i=5;
+    int *p;
+    p=&i;
+    
+    
+    printf("%d \n", p); //add
+    printf("%d \n", *p);//value
+    
+    int a[5]={3,5,7,8,6};
+    p=&a[0];
+    printf("%d \n",p); //addr
+    printf("%d \n",*p); //3 
+    
+    //p=p+2;
+    //printf("%d \n",*p); //7
+    
+    for(int k=0; k<5; k++)
+    {
+        printf("%d \n",*p);
+        p++;
+    }
 
-   int  var1;
-   char var2[10];
-
-   printf("Address of var1 variable: %x\n", &var1  );
-   printf("Address of var2 variable: %x\n", &var2  );
-
-   return 0;
+    return 0;
 }
