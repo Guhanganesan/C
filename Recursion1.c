8263d5ee096aa39a7cbe0aab9c64fae7078be58c
Guhan Ganesan <bharadhiguhan@ymail.com>
2019-08-07 17:32:46 +0530

    Create Recursion1.c

---
+++ Recursion1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int test()
+{
+    for(int i=0; i<=6000; i++)
+    {
+        
+    }
+    printf("Hi \n");
+    test();
+}
+
+int main()
+{
+   
+    test();
+    return 0;
+}
