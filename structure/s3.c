cc4974d7a97bb8c65ec4d0d9baffaa5315bca964
Guhan Ganesan <bharadhiguhan@ymail.com>
2019-08-09 18:03:30 +0530

    Update s3.c

--- structure/s3.c
+++ structure/s3.c
@@ -1,17 +1,67 @@
-#include <stdio.h>
-#include <string.h>
-#include "structure.h"   /* header file where C structure is 
-                            declared */
- 
-int main() 
-{   
- 
-   record.id=1;
-   strcpy(record.name, "Raju");
-   record.percentage = 86.5;
- 
-   printf(" Id is: %d \n", record.id);
-   printf(" Name is: %s \n", record.name);
-   printf(" Percentage is: %f \n", record.percentage);
-   return 0;
+#include<stdio.h>
+#include<string.h>
+
+struct student
+{
+	char name[50];
+	char id[50];
+	int age;
+	
+};
+
+int main()
+{
+   struct student std[2];
+   char temp[50]; int k;
+   
+   
+  for(int i=0; i<2; i++)
+  {
+	   printf("Enter Name \n");
+	   scanf("%s",&temp);
+	   strcpy(std[i].name,temp);
+	   printf("Enter Id \n");
+	   scanf("%s",&temp);
+	   strcpy(std[i].id,temp);
+	   printf("Enter Age \n");
+	   scanf("%d",&k);
+	   std[i].age=k;
+	   
+  }
+   
+   
+   
+
+   for(int i=0; i<2; i++)
+   {
+	   printf("Name is: %s \n",std[i].name);
+	   printf("Enter Id is:%s \n",std[i].id);
+	   printf("Enter Age is:%d \n",std[i].age);
+   }
+   
+   
+return 0;
 }
+
+/*
+
+Answer:-
+Enter Name 
+Guhan
+Enter Id 
+A001
+Enter Age 
+27
+Enter Name 
+Anbu
+Enter Id 
+A002
+Enter Age 
+21
+Name is: Guhan 
+Enter Id is:A001 
+Enter Age is:27 
+Name is: Anbu 
+Enter Id is:A002 
+Enter Age is:21 
+*/
